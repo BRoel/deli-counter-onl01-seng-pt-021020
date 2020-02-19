4bd3e766f51bb1717ad4acb430921c79464e8142
Brennan Roel <brennanroel@gmail.com>
2020-02-19 03:27:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(array)
+ current_line = []
+     puts "The line is currently empty."
+   else
+     array.each do |name|
+       customers = "The line is currently: #{num}. #{name}"
+       num += 1
+       current_line << customers
+   end
+     return current_line
+ end
+end
