d562d3b0972cce06ab336a7f258f6d9cc4a184eb
Brennan Roel <brennanroel@gmail.com>
2020-02-19 03:34:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,12 @@
-def line(array)
- current_line = []
-   if current_line.size == 0
-     puts "The line is currently empty."
-   else
-     array.each do |name|
-       customers = "The line is currently: #{num}. #{name}"
-       num += 1
-       current_line << customers
-   end
-     return current_line
- end
-end
+function currentLine(cLOP) {
+ var str ="The line is currently:"
+ if (cLOP[0]===undefined){
+   str ="The line is currently empty."
+ }
+ else {
+ for (var i=0;i<cLOP.length;i++){
+   str=str +  ${i+1}. ${cLOP[i]},
+ }
+ }
+ return str
+}
