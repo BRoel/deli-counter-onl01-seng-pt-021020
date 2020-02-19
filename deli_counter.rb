57ee048009c644915f0f2e1bfb9eb4744da2f6fb
Brennan Roel <brennanroel@gmail.com>
2020-02-19 03:40:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++
@@ -1,7 +0,0 @@
-function addToCart(item) 
-  var newObj = {};
-  var itemName = `{itemName:"${item}"`;
-  var itemPrice =`itemPrice: ${Math.floor(100*Math.random())}}`;
-  newObj[itemName] = itemPrice;
-  cart.push(newObj);
-  return `${item} has been added to your cart.`;
