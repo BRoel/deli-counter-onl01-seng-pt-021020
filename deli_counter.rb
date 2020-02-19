f05319eb4353f067ec4dd1f81b163652d5ada583
Brennan Roel <brennanroel@gmail.com>
2020-02-19 03:38:37 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,7 @@
+function addToCart(item) 
+  {var newObj = {};
+  var itemName = `{itemName:"${item}"`;
+  var itemPrice =`itemPrice: ${Math.floor(100*Math.random())}}`;
+  newObj[itemName] = itemPrice;
+  cart.push(newObj);
+  return `${item} has been added to your cart.`;}
